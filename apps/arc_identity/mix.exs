d34251ee4dd35506c5efd66f594513bddd344424
Gezim Basha <gezimmb@gmail.com>
2026-09-18 15:10:52 +0200

    chore(release): prepare v0.5.0 (#46)

--- apps/arc_identity/mix.exs
+++ apps/arc_identity/mix.exs
@@ -4,7 +4,7 @@ defmodule Arc.Identity.MixProject do
   def project do
     [
       app: :arc_identity,
-      version: "0.4.1",
+      version: "0.5.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
